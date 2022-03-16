868a927ec93a6c2997d9aeb8d07f86018421b2af
Roko Fumić <60196246+rfumic@users.noreply.github.com>
2022-03-16 20:23:46 +0100

    init

---
+++ src/views/DetailsView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="sdf">
+    {{ params }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+  props: {},
+  data() {
+    return {
+      params: this.$route.params,
+    };
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="text-4xl">Vue.js Commits</h1>
+  <div class="bg-blue-200 flex flex-col items-center m-5 p-5">
+    <div
+      class="border border-black w-1/2 m-1 bg-white text-xl hover:cursor-pointer hover:text-blue-500"
+      v-for="commit in dataArray"
+      :key="commit.sha"
+      @click="goToDetails(commit)"
+    >
+      {{ commit.sha }}
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  props: {},
+  data() {
+    return {
+      dataArray: [],
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    async getData() {
+      try {
+        const response = await fetch(
+          "https://api.github.com/repos/vuejs/vue/commits"
+        );
+
+        const result = await response.json();
+        console.log(result);
+        this.dataArray = result;
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    goToDetails(params) {
+      console.log(params);
+      this.$router.push({
+        name: "DetailsView",
+        params: params,
+      });
+    },
+  },
+};
 </script>
